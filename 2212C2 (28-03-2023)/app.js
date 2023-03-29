e100af3bcac97482fdf9283f828e32fd09d4e154
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-29 14:15:38 +0500

    Add files via upload

---
+++ 2212C2 (28-03-2023)/app.js
@@ -0,0 +1,114 @@
+// function add(){
+//     let num1=+prompt("Enter first number")
+//     let num2=+prompt("Enter second number")
+//     let sum=num1+num2
+//     alert("Sum is "+sum)
+// }
+
+// alert("First time")
+// add()
+// alert("Second time")
+// add()
+// alert("Third time")
+// add()
+
+// for(let i=0;i<5;i++){
+//     add()
+// }
+
+// alert("Welcome to ABC Bank")
+// let balance=10000
+
+// function withdraw(){
+//     let amount=+prompt("Enter amount to withdraw")
+//     if(amount>balance){
+//         alert("Insufficient Balance")
+//     }
+//     else{
+//         alert("Balance before withdraw: "+balance)
+//         balance=balance-amount
+//         alert("Withdraw Successful")
+//         alert("Current Balance is: "+balance)
+//     }
+// }
+// function deposit(){
+//     let amount=+prompt("Enter amount to deposit")
+//     alert("Balance before deposit: "+balance)
+//     balance=balance+amount
+//     alert("Deposit Successful")
+//     alert("Current Balance is: "+balance)
+
+// }
+
+// let user=+prompt("Enter 1 to withdraw. Enter 2 to deposit.")
+
+// switch(user){
+//     case 1:
+//         withdraw()
+//         break
+//     case 2:
+//         deposit()
+//         break
+//     default:
+//         alert("Invalid Input")
+//         break
+// }
+
+// alert("Calculator")
+
+// function add(){
+//     let sum=num1+num2
+//     alert("Sum is "+sum)
+// }
+// function sub(){
+//     let sub=num1-num2
+//     alert("Subtraction is "+sub)
+// }
+// function mul(){
+//     let mul=num1*num2
+//     alert("Multiplication is "+mul)
+// }
+// function div(){
+//     if(num2===0){
+//         alert("Cannot divide by zero")
+//     }
+//     else{
+//     let div=num1/num2
+//     alert("Division is "+div)
+// }
+// }
+
+// let user=prompt("Enter + to add. Enter - to subtract. Enter * to multiply. Enter / to divide.")
+// let user=prompt("+, - , *, /")
+// let num1=+prompt("Enter first number")
+// let num2=+prompt("Enter second number")
+
+
+// if(user==="+"){
+//     add()
+// }
+// else if(user==="-"){
+//     sub()
+// }
+// else if(user==="*"){
+//     mul()
+// }
+// else if(user=== "/"){
+//     div()
+// }
+// else{
+//     alert("Invalid Input")
+// }
+
+// function findEligibility(){
+//     let age=+prompt("Enter age")
+//     let city=prompt("Enter city")
+//     if(age>=18 && city==="Karachi"){
+//         alert("Eligible")
+//     }
+//     else{
+//         alert("Not Eligible")
+//     }
+// }
+
+
